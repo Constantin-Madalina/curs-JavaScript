a112152fe27bc1c211d8addacdf563f767174df9
Constantin-Mada <madalinapetrescu20@yahoo.com>
2019-04-16 18:02:17 +0300

    tema

--- Sedinta04/ex14/app.js
+++ Sedinta04/ex14/app.js
@@ -20,3 +20,31 @@ var person = {
     age: 10
   }]
 };
+console.log('----------Ex01----------------');
+
+person.pets.forEach(element => {
+  console.log(element.name);
+});
+
+console.log('----------Ex02----------------');
+
+var s=0;
+for(i=0;i<person.pets.length;i++){
+  s+=person.pets[i].age;
+}
+console.log('Suma anilor este '+ s);
+
+console.log('----------Ex03----------------');
+person.pets.forEach(element => {
+  console.log(element.name+' este '+ element.species+ ' si are '+element.age+' ani');
+  
+});
+console.log('----------Ex04----------------');
+var dif=0;
+
+
+for(i=0;i<person.pets.length;i++){
+  dif=(2019-person.birthYear)-person.pets[i].age;
+ console.log('Intre Dragos si '+ person.pets[i].name +' este o diferenta de '+ dif+' ani.');
+   
+}
